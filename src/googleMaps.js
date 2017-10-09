411d6a07214b7beefa582bf98cb2ddeff3e87ebc
hector <hector.j.manriquez@gmail.com>
2017-10-09 13:14:44 -0500

    Adding a google maps node client

--- src/googleMaps.js
+++ src/googleMaps.js
@@ -1,2 +1,11 @@
+import { createClient } from '@google/maps'
+
 export const key = process.env.GOOGLE_MAPS_API_KEY;
 export const url = `https://maps.googleapis.com/maps/api/js?key=${key}&v=3.exp&libraries=places`;
+
+const googleMapsClient = createClient({
+  key,
+  Promise,
+});
+
+export default googleMapsClient;
